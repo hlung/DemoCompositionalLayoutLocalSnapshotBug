b1f041e647c13e2b4ff906fc265a7d687425d5bc
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2022-12-02 18:23:47 +0700

    Initial Commit

---
+++ TryDiffableLayoutCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryDiffableLayoutCollectionView
+//
+//  Created by Kolyutsakul, Thongchai on 2/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
