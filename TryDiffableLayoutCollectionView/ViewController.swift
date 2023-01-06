ccf2f639f16644cd42a85176f8fee210bfac5ee2
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2023-01-06 10:23:52 +0800

    Clean up

--- TryDiffableLayoutCollectionView/ViewController.swift
+++ TryDiffableLayoutCollectionView/ViewController.swift
@@ -14,11 +14,18 @@ class ViewController: UIViewController {
 
   var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
 
-  private lazy var dataSource: UICollectionViewDiffableDataSource<Section, Item> = {
+  private lazy var collectionView: UICollectionView = {
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.automaticallyAdjustsScrollIndicatorInsets = true
+    collectionView.backgroundColor = .clear
+    return collectionView
+  }()
 
+  private lazy var dataSource: UICollectionViewDiffableDataSource<Section, Item> = {
     let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Int> { cell, indexPath, item in
       var contentConfiguration = cell.defaultContentConfiguration()
-      contentConfiguration.text = "\(item) (\(Int.random(in: 1...100)))"
+      contentConfiguration.text = "\(item) (random: \(Int.random(in: 1...100)))"
       cell.layer.borderWidth = 1
       cell.layer.borderColor = UIColor.gray.cgColor
       cell.contentConfiguration = contentConfiguration
@@ -33,30 +40,22 @@ class ViewController: UIViewController {
                                                             item: item)
       }
     )
-
-  }()
-
-  private lazy var collectionView: UICollectionView = {
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    collectionView.automaticallyAdjustsScrollIndicatorInsets = true
-    collectionView.backgroundColor = .clear
-    return collectionView
   }()
 
   private lazy var layout: UICollectionViewCompositionalLayout = {
     UICollectionViewCompositionalLayout { [weak self] (sectionIndex, environment) -> NSCollectionLayoutSection? in
       guard let self = self else { return nil }
 
-      print("UICollectionViewCompositionalLayout sectionIndex \(sectionIndex)")
+      print("layout sectionIndex \(sectionIndex)")
 
       // After snapshot.reloadSections
 
-      // This crashes with index out of bounds
+      // This can crashe with index out of bounds on section delete
       // Note: I know we are currently not using snapshotSection. This is just to simulate when we do.
       let snapshotSection = self.snapshot.sectionIdentifiers[sectionIndex]
 
-      // no crash
+      // Option 2
+      // No crash
 //      let snapshotSection = self.dataSource.snapshot().sectionIdentifiers[sectionIndex]
 
       let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
@@ -67,10 +66,7 @@ class ViewController: UIViewController {
 
       let isRegular = environment.traitCollection.horizontalSizeClass == .regular
       let bottomInset: CGFloat = isRegular ? 24 : 12
-      section.contentInsets = NSDirectionalEdgeInsets(top: 0,
-                                                      leading: 0,
-                                                      bottom: bottomInset,
-                                                      trailing: 0)
+      section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: bottomInset, trailing: 0)
       return section
     }
   }()
@@ -112,6 +108,21 @@ extension ViewController: UICollectionViewDelegate {
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     let sheet = UIAlertController(title: "Menu", message: "\(indexPath)", preferredStyle: .actionSheet)
 
+    sheet.addAction(UIAlertAction(title: "Section delete", style: .destructive, handler: { action in
+      let section = self.snapshot.sectionIdentifiers[indexPath.section]
+
+      // Option 1
+      // no crash
+//      var newSnapshot = self.snapshot
+//      newSnapshot.deleteSections([section])
+//      self.dataSource.apply(newSnapshot, animatingDifferences: false)
+//      self.snapshot = newSnapshot
+
+      // crash
+      self.snapshot.deleteSections([section])
+      self.dataSource.apply(self.snapshot, animatingDifferences: false)
+    }))
+
     sheet.addAction(UIAlertAction(title: "Section reload", style: .default, handler: { action in
       let section = self.snapshot.sectionIdentifiers[indexPath.section]
 
@@ -119,22 +130,10 @@ extension ViewController: UICollectionViewDelegate {
 //      var newSnapshot = self.snapshot
 //      newSnapshot.reloadSections([section])
 //      self.dataSource.apply(newSnapshot, animatingDifferences: false)
+//      self.snapshot = newSnapshot
 
       // crash
       self.snapshot.reloadSections([section])
-      self.dataSource.apply(self.snapshot, animatingDifferences: false)
-    }))
-
-    sheet.addAction(UIAlertAction(title: "Section delete", style: .destructive, handler: { action in
-      let section = self.snapshot.sectionIdentifiers[indexPath.section]
-
-      // no crash
-//      var newSnapshot = self.snapshot
-//      newSnapshot.deleteSections([section])
-//      self.dataSource.apply(newSnapshot, animatingDifferences: false)
-
-      // crash
-      self.snapshot.deleteSections([section])
       self.dataSource.apply(self.snapshot, animatingDifferences: false)
     }))
 
